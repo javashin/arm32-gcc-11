9e1d0df4548f29d889c7df8bca1d90dfacf72f99
Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
2021-05-08 22:07:56 +0000

    Import arm-eabi: Gnu/Gcc-11.1.0 + Gnu/Binutils 2.35.2
    MoonBase Android-ToolCahin Stable.
    
    gcc version 11.1.0 (MoonBase Android-ToolChain)
    +
    GNU ld (jsX-Custom BinUtils) 2.35.2
    
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

---
+++ lib/gcc/arm-eabi/11.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=arm-eabi --prefix=/home/Java/GCC/gcc-build/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='MoonBase Android-ToolChain' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" } };
